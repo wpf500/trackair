6248ed01290fd53821fafdbcc0b160b7cd6acca6
Will Franklin <will.franklin@guardian.co.uk>
2015-06-13 16:10:54 +0100

    traceur update

--- src/js/config.js
+++ src/js/config.js
@@ -19,8 +19,8 @@ System.config({
     "json": "github:systemjs/plugin-json@0.1.0",
     "reqwest": "github:ded/reqwest@1.1.5",
     "text": "github:systemjs/plugin-text@0.0.2",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.87",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87"
+    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88"
   }
 });
 
